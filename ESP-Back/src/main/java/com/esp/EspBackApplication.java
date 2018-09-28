40f69a2d8abb8fd3c0a32d2442d6a1d4828ebabd
Pena <Luis.A.Pena@unisys.com>
2018-09-28 06:27:37 -0500

    Primer Commit Back-end

---
+++ ESP-Back/src/main/java/com/esp/EspBackApplication.java
@@ -0,0 +1,12 @@
+package com.esp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspBackApplication.class, args);
+	}
+}
---
+++ ESP-Back/src/test/java/com/esp/EspBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.esp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EspBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
